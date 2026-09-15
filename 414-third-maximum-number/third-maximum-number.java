be639f429b3b94ceb1172543d33433dae9a1dd51
Devashish Srivastava <devashishsri08@gmail.com>
2026-09-16 00:49:26 +0530

    Time: 1 ms (98.83%) | Memory: 44.7 MB (66.55%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,74 +1,74 @@
-// class Solution {
-//     public int thirdMax(int[] num) {
-//         long V1 = Long.MIN_VALUE;
-//            long V2 = Long.MIN_VALUE;
-//               long V3 = Long.MIN_VALUE;
-//       for(int  i = 0 ; i < num.length ; i++){
-//         if(num[i ] == V1  || num[i] == V2  || num[i] == V3){
-//             continue;
-//         }
-//         if(num[i] > V1){
-//             V3 = V2;
-//             V2 = V1 ;
-//             V1 = num[i];
-//         }
-//         else if(num[i] > V2){
-//             V3 = V2;
-//             V2 = num[i];
-//         }
-//         else if(num[i] > V3){
-//             V3 = num[i];
-//         }
-//       }
-//         if( V3 == Integer.MIN_VALUE){
-//             return (int)V1;
-//         }
-//         else{
-//             return (int) V3; 
-//         }
-//     }
-// }
+class Solution {
+    public int thirdMax(int[] num) {
+        long V1 = Long.MIN_VALUE;
+           long V2 = Long.MIN_VALUE;
+              long V3 = Long.MIN_VALUE;
+      for(int  i = 0 ; i < num.length ; i++){
+        if(num[i ] == V1  || num[i] == V2  || num[i] == V3){
+            continue;
+        }
+        if(num[i] > V1){
+            V3 = V2;
+            V2 = V1 ;
+            V1 = num[i];
+        }
+        else if(num[i] > V2){
+            V3 = V2;
+            V2 = num[i];
+        }
+        else if(num[i] > V3){
+            V3 = num[i];
+        }
+      }
+        if( V3 == Long.MIN_VALUE){
+            return (int)V1;
+        }
+        else{
+            return (int) V3; 
+        }
+    }
+}
 
 //  17/35 testcase pss ho rhe h yhe code me jo upar h 
 
-class Solution {
+// class Solution {
 
-    public int thirdMax(int[] nums) {
+//     public int thirdMax(int[] nums) {
 
-        long V1 = Long.MIN_VALUE;
-        long V2 = Long.MIN_VALUE;
-        long V3 = Long.MIN_VALUE;
+//         long V1 = Long.MIN_VALUE;
+//         long V2 = Long.MIN_VALUE;
+//         long V3 = Long.MIN_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
+//         for (int i = 0; i < nums.length; i++) {
 
-            if (nums[i] == V1 || nums[i] == V2 || nums[i] == V3) {
-                continue;
-            }
+//             if (nums[i] == V1 || nums[i] == V2 || nums[i] == V3) {
+//                 continue;
+//             }
 
-            if (nums[i] > V1) {
+//             if (nums[i] > V1) {
 
-                V3 = V2;
-                V2 = V1;
-                V1 = nums[i];
+//                 V3 = V2;
+//                 V2 = V1;
+//                 V1 = nums[i];
 
-            } 
-            else if (nums[i] > V2) {
+//             } 
+//             else if (nums[i] > V2) {
 
-                V3 = V2;
-                V2 = nums[i];
+//                 V3 = V2;
+//                 V2 = nums[i];
 
-            } 
-            else if (nums[i] > V3) {
+//             } 
+//             else if (nums[i] > V3) {
 
-                V3 = nums[i];
-            }
-        }
+//                 V3 = nums[i];
+//             }
+//         }
 
-        // Third maximum doesn't exist
-        if (V3 == Long.MIN_VALUE) {
-            return (int)V1;
-        }
+       
+//         if (V3 == Long.MIN_VALUE) {
+//             return (int)V1;
+//         }
 
-        return (int)V3;
-    }
-}
+//         return (int)V3;
+//     }
+// }
